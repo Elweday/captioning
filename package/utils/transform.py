7c339f19b8a21fe8e0476509f682d1627ddf71c0
elweday <elweday22@gmail.com>
2024-03-21 23:30:38 +0200

    I'm the best programmer ever existed

--- package/utils/transform.py
+++ package/utils/transform.py
@@ -3,23 +3,19 @@ from moviepy.editor import TextClip, CompositeVideoClip, concatenate_videoclips,
 import json
 import ffmpeg    
 import random
+import cv2
 
-def round_corners(image, radius):
-    height, width, channels = image.shape
-    y, x = np.ogrid[0:height, 0:width]
-    mask = np.ones((height, width))
+def rounded_mask(width, height, radius):
+    img = np.zeros((height, width, 3), dtype=np.uint8)
+    WHITE = (255, 255, 255)
+    cv2.rectangle(img, (0, radius), (width, height - radius), WHITE, -1)
+    cv2.rectangle(img, (radius, 0), (width - radius, height), WHITE, -1)
+    cv2.circle(img, (radius, radius), radius, WHITE, -1)
+    cv2.circle(img, (width - radius, radius), radius, WHITE, -1)
+    cv2.circle(img, (radius, height - radius), radius, WHITE, -1)
+    cv2.circle(img, (width - radius, height - radius), radius, WHITE, -1)
+    return img
 
-    # Calculate distance from center for each pixel
-    x_center, y_center = width / 2, height / 2
-    distance_from_center = np.sqrt((x - x_center)**2 + (y - y_center)**2)
-
-    # Apply mask to create rounded corners
-    mask[distance_from_center <= radius] = 0
-
-    # Multiply the image with the mask
-    rounded_image = image * mask[:,:,np.newaxis]
-
-    return rounded_image
 
 
 
@@ -27,15 +23,12 @@ def random_color():
     return (int(random.random()* 255), int(random.random()* 255), int(random.random()* 255))
 
 
-def makeUpdater(scale, duration, initial_width, initial_height, initial_x, initial_y):
+def makeUpdater(scale, duration, initial_width, initial_height, initial_x, initial_y, damping, stiffness):
     initial_scale = 1
-    final_width = initial_width
-    final_height = initial_height
-    
+    final_width = initial_width * initial_scale
+    final_height = initial_height * initial_scale
     def scaleUpdater(t):
         start_scale = initial_scale
-        damping = 0.8  # Damping factor for the spring
-        stiffness = 0.2  # Stiffness of the spring
         delta_scale = scale - start_scale
         spring_force = -stiffness * delta_scale
         damping_force = -damping * t * delta_scale / duration
@@ -47,20 +40,12 @@ def makeUpdater(scale, duration, initial_width, initial_height, initial_x, initi
     
     def positionUpdater(t):
         scale_factor = scaleUpdater(t)
-        
-        # Calculate the new width and height based on the scale factor
         new_width = initial_width * scale_factor
         new_height = initial_height * scale_factor
-        
-        # Calculate the offset to center the object
         x_offset = (final_width - new_width) / 2
         y_offset = (final_height - new_height) / 2
-        
-        # Calculate the new position
         new_x = initial_x + x_offset
         new_y = initial_y + y_offset
-        
-        # Return position
         return new_x, new_y
     
     return scaleUpdater, positionUpdater
@@ -155,13 +140,17 @@ def create_caption(textJSON, framesize, **kwargs):
     highlight_color = kwargs.get('highlight_color', 'yellow')
     bg_color = kwargs.get('bg_color', (150, 0, 100))
     bg_opacity = kwargs.get('bg_opacity', 1)
-    bg_scaling = kwargs.get('bg_scaling', 1.8)
+    bg_scaling = kwargs.get('bg_scaling_factor', 1.2)
+    damping = kwargs.get('bg_scaling_damping', 0.8)
+    stiffness = kwargs.get('bg_scaling_stiffness', 0.2)
     bg_x_padding = kwargs.get('bg_x_padding', 25)
     bg_y_padding = kwargs.get('bg_y_padding', 15)
+    bg_border_radius = kwargs.get('bg_border_radius', 15)
     stroke_color = kwargs.get('stroke_color', 'black')
     stroke_width = kwargs.get('stroke_width', 25)
     spacing = kwargs.get('spacing', 1.5)
     frame_padding = kwargs.get('frame_padding', 0.07)
+    
     
     wordcount = len(textJSON['textcontents'])
     full_duration = textJSON['end']-textJSON['start']
@@ -197,8 +186,6 @@ def create_caption(textJSON, framesize, **kwargs):
         word, start, end = wordJSON['word'], wordJSON['start'], wordJSON['end']
         dt = end - start
         
-
-            
         word_clip = word_clip.set_position((x_pos, y_pos))
         word_clip_stroke = word_clip_stroke.set_position((x_pos - stroke_width/2, y_pos - stroke_width/2))
         word_clip_space = word_clip_space.set_position((x_pos+ word_width , y_pos))
@@ -210,25 +197,26 @@ def create_caption(textJSON, framesize, **kwargs):
         x_init = x_pos - bg_x_padding//2
         y_init = y_pos - bg_y_padding//2
         size = (w_init, h_init)
-        scaleFunc, posFunc = makeUpdater(bg_scaling, dt, w_init, h_init, x_init , y_init)
+        scaleFunc, posFunc = makeUpdater(bg_scaling, dt, w_init, h_init, x_init , y_init, damping, stiffness)
+        mask = ImageClip(rounded_mask(w_init, h_init, bg_border_radius), duration=duration, ismask=True)
         color_clip = ColorClip(size=(size), color= bg_color) \
           .set_opacity(bg_opacity) \
           .set_position(posFunc) \
           .resize(scaleFunc) \
           .set_start(start) \
-          .set_duration(dt)
+          .set_duration(dt) \
+          .set_mask(mask)
           
         line += [color_clip, word_clip_stroke, word_clip, word_clip_space, word_clip_highlight]
         
         x_pos += word_width + space_width
         line_width += word_width + space_width
-        if (line_width + word_width+ space_width > max_line_width) or (index == wordcount):
+        if (line_width + word_width+ space_width > max_line_width) or (index == wordcount-1):
             # next line
             lines.append({"line": line, "width": line_width, "height": word_height})
             line = []
             x_pos = bg_x_padding//2
-            y_pos += word_height + bg_y_padding//2
-            line_width = word_width + space_width
+            line_width = 0
 
     return lines, word_height
 
@@ -245,17 +233,15 @@ def create_video_from_subtitles(videofilename, timestamps, **kwargs):
         pos_y = y_padding//2
         for line_num, line in enumerate(lines):
             out_clips, width, height = line["line"], line["width"], line["height"]
-            pos_x = (max_width - width)//2 + x_padding//2
-            pos_y += (height + y_padding//2)
-            clip_to_overlay = (CompositeVideoClip(out_clips, size=(width, height))
-                               .set_position((pos_x, pos_y))
-            )
+            pos_x = (x_padding + max_width - width)//2
+            pos_y += height
+            clip_to_overlay = CompositeVideoClip(out_clips, size=(max_width, max_height)).set_position((pos_x, pos_y))
             all_linelevel_splits.append(clip_to_overlay)
     
     input_video_duration = input_video.duration
     final_video = CompositeVideoClip([input_video] + all_linelevel_splits)
     final_video = final_video.set_audio(input_video.audio)
-    final_video = final_video.subclip(0, 6) # TODO: remove
+    final_video = final_video.subclip(0, 8) # TODO: remove
     output_filename = videofilename.replace(".mp4","_output.mp4")
     
     final_video.write_videofile(output_filename, fps=input_video.fps, codec="libx264", audio_codec="aac")
