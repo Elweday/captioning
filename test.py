dc2155e142c25072618951a6b01a1ee1e17f804f
elweday <elweday22@gmail.com>
2024-04-16 19:59:43 +0200

    no artifact registry

--- test.py
+++ test.py
@@ -10,9 +10,9 @@ with open("sample_output.mp4", "rb") as f:
     fo.write(content)
 
 
-  
 video_clip = mpe.VideoFileClip(fo.name)
-text_clip = mpe.TextClip("Watermark", fontsize=70, color='white').set_duration(2)
+text_clip = mpe.TextClip("Watermark", fontsize=70,
+                         color='white').set_duration(2)
 text_clip = text_clip.set_position('center')
 watermarked_video = mpe.CompositeVideoClip([video_clip, text_clip])
 watermarked_video.write_videofile(fo.name,  codec='libx264')
@@ -20,7 +20,6 @@ watermarked_video.write_videofile(fo.name,  codec='libx264')
 with open("test_file_test.mp4", "wb") as output_file:
     print("should have been written")
     f.write(fo.read())
-
 
 
 fo.close()
