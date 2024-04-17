a8169cd82605509f7d1d00b0f2fc23d658d6b1ce
elweday <elweday22@gmail.com>
2024-04-18 00:00:50 +0200

    turn off debug mode

--- server.py
+++ server.py
@@ -9,16 +9,16 @@ from app.utils import convert_array, create_video_from_subtitles
 app = Flask(__name__)
 
 
-@app.route('/process_video', methods=['POST'])
+@app.route('/', methods=['POST'])
 def process_video():
     # Check if the request contains a file
-    if 'file' not in request.files:
+    if 'video' not in request.files:
         return 'No file uploaded', 400
 
     if 'timestamps' not in request.files:
         return 'No data uploaded', 400
 
-    uploaded_file = request.files['file']
+    uploaded_file = request.files['video']
     if uploaded_file.filename == '':
         return 'No file selected', 400
 
@@ -34,16 +34,12 @@ def process_video():
     # Save the uploaded file
     uploaded_file.save(filename)
     output_filename = create_video_from_subtitles(
-        filename, convert_array(time_stamps), **options)
+        filename, convert_array(time_stamps),
+        **options)
 
     # Return the processed video file
     return send_file(output_filename, mimetype='video/mp4')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-# read json
-with open('time_stamps.json') as json_file:
-    data = json.load(json_file)
+    app.run()
